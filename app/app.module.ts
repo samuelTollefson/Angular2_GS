4d01a7bdf20a06746d9b8937d239183c159c3522
samuelTollefson <samueltollefson@gmail.com>
2017-06-06 10:45:34 -0500

    pre-services-all

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,8 @@ import {FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component' ;
+import { ProductFilterPipe } from './products/product-filter.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
 imports: [ 
@@ -12,7 +14,10 @@ imports: [
 ],
 declarations: [ 
   AppComponent ,
-  ProductListComponent],
+  ProductListComponent,
+  ProductFilterPipe,
+  StarComponent 
+  ],
 bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- app/products/product-list.component.js
+++ app/products/product-list.component.js
@@ -13,7 +13,7 @@ var ProductListComponent = (function () {
         this.imageWidth = 50;
         this.imageMargin = 2;
         this.showImage = false;
-        this.listFilter = 'cart';
+        this.listFilter = '';
         this.products = [
             {
                 "productId": 2,
@@ -40,12 +40,20 @@ var ProductListComponent = (function () {
     ProductListComponent.prototype.toggleImage = function () {
         this.showImage = !this.showImage;
     };
+    ProductListComponent.prototype.ngOnInit = function () {
+        console.log('In OnInit');
+    };
+    ProductListComponent.prototype.onRatingClicked = function (message) {
+        this.pageTitle = 'Product List: ' + message;
+    };
     return ProductListComponent;
 }());
 ProductListComponent = __decorate([
     core_1.Component({
         selector: 'pm-products',
-        templateUrl: 'app/products/product-list.component.html'
+        moduleId: module.id,
+        templateUrl: 'product-list.component.html',
+        styleUrls: ['product-list.component.css']
     })
 ], ProductListComponent);
 exports.ProductListComponent = ProductListComponent;
